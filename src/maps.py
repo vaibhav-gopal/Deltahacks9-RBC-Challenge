b9182ab75da431ddd61f5bb13a24a1d232b1c3f0
Vaibhav Gopal <vabsgop@gmail.com>
2023-01-14 15:03:34 -0500

    Search websites for rental listings

--- src/maps.py
+++ src/maps.py
@@ -1,4 +1,13 @@
-print('Hello world')
-print("test1")
+import requests
+from bs4 import BeautifulSoup
 
-print("hi")
+url = 'https://www.example.com/rentals'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+rentals = soup.find_all(class_='rental-listing')
+for rental in rentals:
+    title = rental.find(class_='title').get_text()
+    location = rental.find(class_='location').get_text()
+    price = rental.find(class_='price').get_text()
+    print(title + ' | ' + location + ' | ' + price)
