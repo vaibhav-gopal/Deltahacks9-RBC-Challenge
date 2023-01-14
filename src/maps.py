378d0b9041ecbf03cd55dd4bb9f4a04967d96d39
Vaibhav Gopal <vabsgop@gmail.com>
2023-01-14 16:44:17 -0500

    cleanup

--- src/maps.py
+++ src/maps.py
@@ -2,12 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from osm_runner import Runner
 
-# from arcgis.features import FeatureLayer, GeoAccessor, GeoSeriesAccessor
-# from arcgis.geoenrichment import enrich
-# from arcgis import dissolve_boundaries
-# from arcgis.geometry import project
-# from arcgis.gis import GIS
-
 url = 'https://www.kijiji.ca/b-apartments-condos/hamilton/c37l80014'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -17,5 +11,3 @@ for rental in rentals:
     title = rental.find(class_='title').get_text().strip()
     price = rental.find(class_='price').get_text().strip()
     print(title + ' | ' + price)
-
-# gis = GIS('home')
