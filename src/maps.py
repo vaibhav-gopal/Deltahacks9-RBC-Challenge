bd0c70c8e75064c63e43c4566ebee8d8955e0724
Vaibhav Gopal <vabsgop@gmail.com>
2023-01-14 16:23:55 -0500

    First trial run

--- src/maps.py
+++ src/maps.py
@@ -2,21 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 from osm_runner import Runner
 
-from arcgis.features import FeatureLayer, GeoAccessor, GeoSeriesAccessor
-from arcgis.geoenrichment import enrich
-from arcgis import dissolve_boundaries
-from arcgis.geometry import project
-from arcgis.gis import GIS
+# from arcgis.features import FeatureLayer, GeoAccessor, GeoSeriesAccessor
+# from arcgis.geoenrichment import enrich
+# from arcgis import dissolve_boundaries
+# from arcgis.geometry import project
+# from arcgis.gis import GIS
 
-url = 'https://www.example.com/rentals'
+url = 'https://www.kijiji.ca/b-apartments-condos/hamilton/c37l80014'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-rentals = soup.find_all(class_='rental-listing')
+rentals = soup.find_all(class_='info-container')
 for rental in rentals:
     title = rental.find(class_='title').get_text()
-    location = rental.find(class_='location').get_text()
     price = rental.find(class_='price').get_text()
-    print(title + ' | ' + location + ' | ' + price)
+    print(title + ' | ' + price)
 
-gis = GIS('home')
+# gis = GIS('home')
