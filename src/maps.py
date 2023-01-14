e151c744a9c9f61e0fd847cc1d95f191ead52b4b
John Cho <johnnychox@gmail.com>
2023-01-14 17:29:47 -0500

    small changes

--- src/maps.py
+++ src/maps.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 import pandas as pd
 import folium
+import os
 
 url = 'https://www.kijiji.ca/b-apartments-condos/hamilton/c37l80014'
 page = requests.get(url)
@@ -12,16 +13,20 @@ rentals = soup.find_all(class_='info-container')
 for rental in rentals:
     title = rental.find(class_='title').get_text().strip()
     price = rental.find(class_='price').get_text().strip()
-    # print(title + ' | ' + price)
+    print(title + ' | ' + price)
 
-path = Path(__file__).resolve().parent/'location.csv'
-housing = pd.read_csv(path)
+source_path = Path(__file__).resolve().parent
+path = source_path/'location.csv'
+houses = pd.read_csv(path)
 
 center = [-0.023559, 37.9061928]
 map_kenya = folium.Map(location=center, zoom_start=8)
-for index, franchise in housing.iterrows():
-    location = [franchise['latitude'], franchise['longitude']]
-    folium.Marker(location, popup = f'Name:{franchise["store"]}\n Revenue($):{franchise["revenue"]}').add_to(map_kenya)
+for index, house in houses.iterrows():
+    location = [house['latitude'], house['longitude']]
+    folium.Marker(location, popup = f'Name:{house["store"]}\n Revenue($):{house["revenue"]}').add_to(map_kenya)
 
-# save map to html file
-map_kenya.save('index.html')
+if __name__ == '__main__':
+    # save map to html file
+    map_kenya.save('index.html')
+    html_path = source_path.parent/'index.html'
+    os.startfile(html_path)
