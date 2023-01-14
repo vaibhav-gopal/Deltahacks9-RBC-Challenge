724d64f06c829cef0d5a1420762f0198b4377c18
John Cho <johnnychox@gmail.com>
2023-01-14 15:12:36 -0500

    adding googlemaps stuff

--- src/maps.py
+++ src/maps.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import googlemaps
+from datetime import datetime
+
+key = 'AIzaSyCOvffCldigqFeMlG5wSfd12lLbeM6LqN0'
 
 url = 'https://www.example.com/rentals'
 page = requests.get(url)
@@ -11,3 +15,26 @@ for rental in rentals:
     location = rental.find(class_='location').get_text()
     price = rental.find(class_='price').get_text()
     print(title + ' | ' + location + ' | ' + price)
+
+gmaps = googlemaps.Client(key=key)
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions(
+    "Sydney Town Hall",
+    "Parramatta, NSW",
+    mode="transit",
+    departure_time=now)
+
+# Validate an address with address validation
+addressvalidation_result =  gmaps.addressvalidation(
+    ['1600 Amphitheatre Pk'], 
+    regionCode='US',
+    locality='Mountain View', 
+    enableUspsCass=True)
