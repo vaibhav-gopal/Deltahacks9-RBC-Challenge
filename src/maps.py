8d8268d0162beaad708c91f7eaf6e4c5aca4f1f2
Vaibhav Gopal <vabsgop@gmail.com>
2023-01-14 23:07:49 -0500

    improvement

--- src/maps.py
+++ src/maps.py
@@ -1,67 +1,93 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.remote.webelement import WebElement
 
 from webdriver_manager.chrome import ChromeDriverManager
 import geocoder
 import time
 
-
-from pathlib import Path
-import pandas as pd
-import folium
-import os, subprocess
-
 #Web Scraping shit —————————————————————————
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-url = "https://www.google.com/maps/search/"
 usrLoc = geocoder.ip('me').latlng
-zoomLevel = 12
-articlePath="//div[@role='article']//a"
-locationPath = "//button[@data-item-id='address']"
-closeListingPath = "//button[@aria-label='Close']"
-locations = []
+try:
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+except:
+    print("Chrome driver is already in use")
 
-def searchParameters():
+class googleMaps:
+    url = "https://www.google.com/maps/search/"
+    zoomLevel = 12
+    articlePath = "//div[@role='article']//a"
+    locationPath = "//button[@data-item-id='address']"
+    closeListingPath = "//button[@aria-label='Close']"
+    locations = []
+
+google = googleMaps()
+tempEl= []
+
+def gotoAndWait(elementOrUrl, path):
+    if (isinstance(elementOrUrl, WebElement)):
+        elementOrUrl.click()
+    else:
+        driver.get(elementOrUrl)
+    time.sleep(0.5)
+    wait = WebDriverWait(driver, 10)
+    try:
+        element = wait.until(EC.presence_of_element_located((By.XPATH, path)))
+        if element.is_displayed():
+            print('Element is present')
+        else:
+            print('Element is present but not visible')
+    except TimeoutException:
+        print("Timeout Exception: Element not found")
+
+def waitUntilClick(path):
+    time.sleep(0.5)
+    wait = WebDriverWait(driver, 10)
+    try:
+        element = wait.until(EC.presence_of_element_located((By.XPATH, path)))
+        if element.is_displayed():
+            driver.find_element(By.XPATH, path).click()
+            print('Element is present')
+        else:
+            print('Element is present but not visible')
+    except TimeoutException:
+        print("Timeout Exception: Element not found")
+
+def waitUntil(path):
+    time.sleep(0.5)
+    wait = WebDriverWait(driver, 10)
+    try:
+        element = wait.until(EC.presence_of_element_located((By.XPATH, path)))
+        if element.is_displayed():
+            print('Element is present')
+        else:
+            print('Element is present but not visible')
+    except TimeoutException:
+        print("Timeout Exception: Element not found")
+
+def searchParametersGoogle():
     search = input("What keyword:")
     search.strip().replace(" ", "+").lower()
     print(usrLoc)
-    return search + "/@" + str(usrLoc[0]) + "," + str(usrLoc[1]) + ',' + str(zoomLevel) + "z"
+    return search + "/@" + str(usrLoc[0]) + "," + str(usrLoc[1]) + ',' + str(google.zoomLevel) + "z"
 
-def search():
-    driver.get(url + searchParameters())
-    WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.XPATH,articlePath).is_displayed())
-    listings = driver.find_elements(By.XPATH, articlePath)
-    print(listings)
+def searchGoogle():
+    gotoAndWait(google.url + searchParametersGoogle(), google.articlePath)
+    listings = driver.find_elements(By.XPATH, google.articlePath)
     for listing in listings:
-        listing.click()
-        WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.XPATH,locationPath).is_displayed())
-        locations.append(driver.find_element(By.XPATH, locationPath).get_attribute("aria-label"))
-        driver.find_element(By.XPATH, closeListingPath).click()
-        driver.implicitly_wait(2)
-    print(locations)
+        print(listing)
+        gotoAndWait(listing, google.locationPath)
+        google.locations.append(driver.find_element(By.XPATH, google.locationPath).get_attribute("aria-label"))
+        waitUntilClick(google.closeListingPath)
+        waitUntil(google.articlePath)
+    print(google.locations)
     driver.quit()
 
-search()
-
-#Map shit ——————————————————————————————
-# source_path = Path(__file__).resolve().parent
-# path = source_path/'location.csv'
-# houses = pd.read_csv(path)
-
-# center = [-0.023559, 37.9061928]
-# map_kenya = folium.Map(location=center, zoom_start=8)
-# for index, house in houses.iterrows():
-#     location = [house['latitude'], house['longitude']]
-#     folium.Marker(location, popup = f'Name:{house["store"]}\n Revenue($):{house["revenue"]}').add_to(map_kenya)
-
-# if __name__ == '__main__':
-#     # save map to html file
-#     map_kenya.save('index.html')
-#     html_path = source_path.parent/'index.html'
-#     os.startfile(html_path)
+searchGoogle()
